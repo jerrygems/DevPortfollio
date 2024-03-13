965f336009fcc96c2e54723bb7acca1eb14669c7
Shubham Jangid <100356532+jerrygems@users.noreply.github.com>
2024-03-13 17:57:46 +0000

    update

--- dev-portfollio/src/App.js
+++ dev-portfollio/src/App.js
@@ -1,20 +1,25 @@
-<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>wahtever</h1>
-      {/* <Parallex />
-      <Description />
-      <Services />
-      <Works />
-      <Quotes /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-=======
->>>>>>> 2253ddf (update)
