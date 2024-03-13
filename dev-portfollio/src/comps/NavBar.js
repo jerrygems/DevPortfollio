a93c16256d123a7066dd88e68b4185d2506ddb06
theanupamkumar1 <anupamkumar7802@gmail.com>
2024-03-13 18:12:38 +0000

    update

--- dev-portfollio/src/comps/NavBar.js
+++ dev-portfollio/src/comps/NavBar.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function whatever() {
+export default function NavBar() {
   return (
     <View>
       <Text>whatever</Text>
