4e4a5e675dbffb0a4f325ff1b3da1daa56e34902
Shubham Jangid <100356532+jerrygems@users.noreply.github.com>
2024-03-13 16:57:45 +0000

    commit

--- dev-portfollio/src/App.js
+++ dev-portfollio/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/NavBar'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>wahtever</h1>
+      {/* <Parallex />
+      <Description />
+      <Services />
+      <Works />
+      <Quotes /> */}
     </div>
   );
 }
