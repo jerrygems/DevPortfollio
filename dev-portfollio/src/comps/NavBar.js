91bb8631408e03a22f007a0766bd533fbdec8918
Shubham Jangid <100356532+jerrygems@users.noreply.github.com>
2024-03-13 18:43:48 +0000

    just tried the AppBar from mui

--- dev-portfollio/src/comps/NavBar.js
+++ dev-portfollio/src/comps/NavBar.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function NavBar() {
+  // const [anchorElNav, setAnchorElNav] = React.useState < null | HTMLElement > (null);
+  // const [anchorElUser, setAnchorElUser] = React.useState < null | HTMLElement > (null);
+  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
+  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
+
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
+
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -36,7 +68,7 @@ function NavBar() {
             </IconButton>
             <Menu
               id="menu-appbar"
-              anchorEl={anchorElNav}
+              // anchorEl={anchorElNav}
               anchorOrigin={{
                 vertical: 'bottom',
                 horizontal: 'left',
@@ -46,7 +78,7 @@ function NavBar() {
                 vertical: 'top',
                 horizontal: 'left',
               }}
-              open={Boolean(anchorElNav)}
+              // open={Boolean(anchorElNav)}
               sx={{
                 display: { xs: 'block', md: 'none' },
               }}
@@ -97,7 +129,7 @@ function NavBar() {
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
-              anchorEl={anchorElUser}
+              // anchorEl={anchorElUser}
               anchorOrigin={{
                 vertical: 'top',
                 horizontal: 'right',
@@ -107,7 +139,7 @@ function NavBar() {
                 vertical: 'top',
                 horizontal: 'right',
               }}
-              open={Boolean(anchorElUser)}
+              // open={Boolean(anchorElUser)}
             >
               {settings.map((setting) => (
                 <MenuItem key={setting}>
