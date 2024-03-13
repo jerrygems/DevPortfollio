291d7931a49894aa4b54bfe7e32c94e23d427193
Shubham Jangid <100356532+jerrygems@users.noreply.github.com>
2024-03-13 18:10:20 +0000

    update

---
+++ dev-portfollio/src/comps/whatever.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function whatever() {
+  return (
+    <View>
+      <Text>whatever</Text>
+    </View>
+  )
+}
